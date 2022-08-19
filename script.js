0409d72df739ee838ed31a76b1890c7b3d730d2c
Cheng-I Lin <23alanl@ast.tc.edu.tw>
2022-08-19 22:50:49 +0800

    add ac and delete

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
-var display = "";
+var display = "0";
 var num = [];
 var math = [];
 var ans = 0.0;
+var clickedSymbol=false;
 
 function keyboard(id) {
-  display += id;
+    clickedSymbol=false;
+    if(display.charAt(0)=="0"){
+        display=id;
+    } else{
+        display += id;
+    }
 }
 
 function addition() {
-  num.push(display);
-  math.push("+");
-  display = "";
+    if(!clickedSymbol){
+        num.push(display);
+        math.push("+");
+        display = "";
+        clickedSymbol=true;
+    }
 }
 
 function subtraction() {
-  num.push(display);
-  math.push("-");
-  display = "";
+    if(!clickedSymbol){
+        num.push(display);
+        math.push("-");
+        display = "";
+        clickedSymbol=true;
+    }
 }
 
 function multiplication() {
-  num.push(display);
-  math.push("*");
-  display = "";
+    if(!clickedSymbol){
+        num.push(display);
+        math.push("*");
+        display = "";
+        clickedSymbol=true;
+    }
 }
 
 function division() {
-  num.push(display);
-  math.push("\/");
-  display = "";
-
+    if(!clickedSymbol){
+        num.push(display);
+        math.push("\/");
+        display = "";
+        clickedSymbol=true;
+    }
 }
 
 function equal() {
@@ -61,11 +78,75 @@ function deleteKey(){
 }
 
 function clearDisplay(){
-    display="";
+    display="0";
     num=[];
     math=[];
 }
 
-setInterval(function(){
+document.addEventListener("keydown",function(key){
+    switch(key.code){
+        case "Digit0":
+            keyboard("0");
+            break;
+        case "Digit1":
+            keyboard("1");
+            break;
+        case "Digit2":
+            keyboard("2");
+            break;
+        case "Digit3":
+            keyboard("3");
+            break;
+        case "Digit4":
+            keyboard("4");
+            break;
+        case "Digit5":
+            keyboard("5");
+            break;
+        case "Digit6":
+            keyboard("6");
+            break;
+        case "Digit7":
+            keyboard("7");
+            break;
+        case "Digit8":
+            keyboard("8");
+            break;
+        case "Digit9":
+            keyboard("9");
+            break;
+        case "Period":
+            keyboard(".");
+            break;
+        case "Backspace":
+            deleteKey();
+            break;
+        case "NumpadAdd":
+            addition();
+            break;
+        case "Minus":
+            subtraction();
+            break;
+        case "NumpadMultiply":
+            multiplication();
+            break;
+        case "Slash":
+            division();
+            break;
+        case "Equal":
+            equal();
+            break;
+        case "Enter":
+            equal();
+            break;
+        default:
+            break;
+    }
+});
+
+setInterval(function() {
+    if(!clickedSymbol&&(display==""||display=="-")){
+        display="0";
+    }
     document.getElementById("display").innerHTML = display;
 });
